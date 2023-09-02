103712c8e3ba744f88a34019b5b44aa7d039dc64
jklimmek <jakubklimek3@gmail.com>
2023-09-03 00:01:44 +0200

    Added documentation for utils files.

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,4 +1,15 @@
 def read_txt(path, skip_blank=True):
+    """
+    Reads a text file and returns a list of lines.
+
+    Parameters:
+        path (str): Path to text file.
+        skip_blank (bool): Whether to skip blank lines.
+
+    Returns:
+        lines (list): List of lines.
+    """
+
     lines = []
     with open(path, 'r', encoding="utf-8") as file:
         for line in file:
@@ -8,6 +19,19 @@ def read_txt(path, skip_blank=True):
     return lines
 
 def save_txt(txt, path, mode="w", skip_blank=True):
+    """
+    Saves a list of lines to a text file.
+
+    Parameters:
+        txt (list): List of lines.
+        path (str): Path to text file.
+        mode (str): Mode to open file.
+        skip_blank (bool): Whether to skip blank lines.
+
+    Returns:
+        None
+    """
+    
     with open(path, mode=mode, encoding='utf-8') as file:
         for line in txt:
             if skip_blank and line == '':
